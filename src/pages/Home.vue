5b977d519da20105293bf4e61585b6160fc3ea9a
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-06-26 19:32:23 +0200

    initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+	<h1>Home</h1>
+	<p>
+		<img src="../assets/logo.png" alt="logo" />
+	</p>
+	<button @click="state.count++">count is: {{ state.count }}</button>
+	<Foo />
+	<p class="virtual">msg from virtual module: {{ foo.msg }}</p>
+	<p class="inter">this will be styled with a font-face</p>
+</template>
+
+<script setup>
+import foo from "@foo";
+import { reactive, defineAsyncComponent } from "vue";
+import "@raythurnevoid/strings-filter";
+const Foo = defineAsyncComponent(() =>
+	import("../components/Foo").then((mod) => mod.Foo)
+);
+function load(file) {
+	return defineAsyncComponent(() => import(`../components/${file}.vue`));
+}
+const state = reactive({ count: 0 });
+</script>
+
+<style scoped>
+h1,
+a {
+	color: green;
+}
+</style>
